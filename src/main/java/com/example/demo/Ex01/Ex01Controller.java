8f9fa0aa84ca1fddcd3aef509e11f2622f89d0e6
Lumisia <zh2171@gmail.com>
2026-02-25 09:13:38 +0900

    [feat] Ex01Controller 생성

---
+++ src/main/java/com/example/demo/Ex01/Ex01Controller.java
@@ -0,0 +1,13 @@
+package com.example.demo.Ex01;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequestMapping("/ex01")
+@RestController
+public class Ex01Controller {
+}
