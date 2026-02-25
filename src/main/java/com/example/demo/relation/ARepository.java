a0074c1fd9fd0f3d8022be74b3a0360bbb04805c
Lumisia <zh2171@gmail.com>
2026-02-25 14:35:50 +0900

    [feat] A랑 B 테스트

--- src/main/java/com/example/demo/relation/ARepository.java
+++ src/main/java/com/example/demo/relation/ARepository.java
@@ -2,7 +2,12 @@ package com.example.demo.relation;
 
 import com.example.demo.relation.model.A;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ARepository extends JpaRepository<A, Long> {
 
+    @Query("Select")
+    Optional<A> findByBList
 }
