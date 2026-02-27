81da08a4f301bbc30dfe8c9956deabe79be4dbb0
Lumisia <zh2171@gmail.com>
2026-02-27 11:52:24 +0900

    [feat] Upload 구현
    # Conflicts:
    #	src/main/java/com/example/demo/utils/UploadUtil.java
    #	src/main/resources/application.yml

--- src/main/java/com/example/demo/Upload/LocalUploadService.java
+++ src/main/java/com/example/demo/Upload/LocalUploadService.java
@@ -1,4 +1,49 @@
 package com.example.demo.Upload;
 
+import com.example.demo.utils.UploadUtil;
+import jakarta.mail.Multipart;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
 public class LocalUploadService implements UploadService{
+    private final UploadUtil uploadUtil;
+
+    private String saveFile(MultipartFile file) {
+        String uploadPath = uploadUtil.makeFolder();
+
+        String filePath = uploadPath +
+                File.separator +
+                UUID.randomUUID() +
+                "_" + file.getOriginalFilename();
+        File saveFile = new File(filePath);
+
+        try {
+            file.transferTo(saveFile);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return filePath;
+    }
+
+    @Override
+    public List<String> upload(List<MultipartFile> fileList) {
+        List<String> uploadPathList = new ArrayList<>();
+
+//        for(MultipartFile file : fileList) {
+//            String uploadPath = saveFile(file);
+//            uploadPathList.add(uploadPath);
+//        }
+//        return uploadPathList;
+
+        return fileList.stream().map(this::saveFile).toList();
+    }
 }
