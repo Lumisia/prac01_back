4fcd75e1b264c6bb03477da6931cd5e8b54d05e9
Lumisia <zh2171@gmail.com>
2026-02-27 12:10:16 +0900

    [feat] Upload 구현

--- src/main/java/com/example/demo/relation/BRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.relation;
-
-import com.example.demo.relation.model.B;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BRepository extends JpaRepository<B, Long> {
-}
