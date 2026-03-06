a20fb9fae146c6ba67ee1a92a95033d298ba6b7e
Lumisia <zh2171@gmail.com>
2026-03-06 12:05:59 +0900

    [Feat] websocket 기초 구현

---
+++ src/main/java/com/example/demo/config/websocket/WebsocketHandler.java
@@ -0,0 +1,8 @@
+package com.example.demo.config.websocket;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebsocketHandler extends TextWebSocketHandler {
+
+
+}
