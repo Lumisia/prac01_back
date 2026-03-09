21b58275f7a4c5c7df4ecb3d137abedf58c8affa
Lumisia <zh2171@gmail.com>
2026-03-09 16:41:33 +0900

    [Feat] AOP 및 Prometheus, Grafana 및 설정함.

---
+++ src/main/java/com/example/demo/aop/SimpleApp.java
@@ -0,0 +1,73 @@
+package com.example.demo.aop;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect     // 관점 : 흩어진 관심사를 하나로 묶은 것
+@Component
+@RequiredArgsConstructor
+public class SimpleApp {
+
+    private final Tracer tracer;
+
+    // 실행될 위치나 시점을 지정
+    //      * 리턴 타입
+    //      com.example.demo.board.. 해당 패키지 및 하위 모든 패키지
+    //      *.*(..) 모든 클래스의 모든 메소드 및 모든 매개변수
+    @Pointcut("execution(* com.example.demo.board..*.*(..))")
+    private void cut() {
+        System.out.println("컷");
+    }
+
+    //  @Before("execution(* com.example.demo.board..*.*(..))")
+    //  Pointcut을 안만들어두면 이렇게 설정해야 함.
+    @Before("cut()") // 포인트 컷에서 지정한 위치의 클래스의 메소드가 실행되기 전에 현재 메소드를 실행
+    // JoinPoint joinPoint : 위치나 시점 특정 클래스, 특정 메소드라는 정보
+    private void before(JoinPoint joinPoint) {
+        Method method = ((MethodSignature)joinPoint.getSignature()).getMethod();
+
+        System.out.println(joinPoint.getSignature());
+        System.out.println(method.getName() + "메소드 실행 전");
+
+    }
+
+    //  @Before("execution(* com.example.demo.board..*.*(..))")
+    //  Pointcut을 안만들어두면 이렇게 설정해야 함.
+    @After("cut()") // 포인트 컷에서 지정한 위치의 클래스의 메소드가 실행되기 전에 현재 메소드를 실행
+    // JoinPoint joinPoint : 위치나 시점 특정 클래스, 특정 메소드라는 정보
+    private void after(JoinPoint joinPoint) {
+        Method method = ((MethodSignature)joinPoint.getSignature()).getMethod();
+
+        System.out.println(joinPoint.getSignature());
+        System.out.println(method.getName() + "메소드 실행 후");
+
+    }
+
+    @Around("cut()") // 포인트 컷에서 지정한 위치의 클래스의 메소드가 실행되기 전에 현재 메소드를 실행
+    // JoinPoint joinPoint : 위치나 시점 특정 클래스, 특정 메소드라는 정보
+    private Object traceMethod(ProceedingJoinPoint joinPoint) throws Throwable{
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+
+        Span span = tracer.spanBuilder(className + "." + methodName).startSpan();
+        try (Scope scope = span.makeCurrent()) {
+            span.setAttribute("method.name", methodName);
+
+            return joinPoint.proceed();
+        }catch (Exception e) {
+            span.recordException(e);
+            throw e;
+        } finally {
+            span.end();
+        }
+    }
+}
